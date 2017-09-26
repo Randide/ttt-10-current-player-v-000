acda1de47d1bba50ef0274dc6967bfee07a28bd7
Randi Nash <randidena@live.com>
2017-09-26 00:32:50 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,13 @@
 board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 
 def turn_count[board]
+  rounds = 0
+  board.each do |playermark|
+    if playermark = "X" || playermark ="O"
+      rounds += 1
+    end
+  end
+  rounds
 end
 
 def current_player[board]
